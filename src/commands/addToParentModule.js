96259691d78e78ecf6813c8ddf2122a929ad3879
Jeff Calaway <hello@jeffcalaway.com>
2024-12-20 11:40:57 -0600

    Bump version to 2.1.1 and simplify parent folder name extraction in addToParentModule command

--- src/commands/addToParentModule.js
+++ src/commands/addToParentModule.js
@@ -1,7 +1,6 @@
-const prompts = require('../utils/prompts');
-const moduleUtil  = require('../utils/module');
-const format  = require('../utils/format');
-const path    = require('path');
+const prompts    = require('../utils/prompts');
+const moduleUtil = require('../utils/module');
+const path       = require('path');
 
 const addToParentModule = async (uri) => {
   if (!uri || !uri.fsPath) {
@@ -12,7 +11,7 @@ const addToParentModule = async (uri) => {
   const childFilePath    = uri.fsPath;
   const childDir         = path.dirname(childFilePath);
   const parentDir        = path.dirname(childDir);
-  const parentFolderName = format.toPlural(path.basename(childDir));
+  const parentFolderName = path.basename(childDir);
   const parentFileName   = `class-${parentFolderName}.php`;
   const parentFilePath   = path.join(parentDir, parentFileName);
 
